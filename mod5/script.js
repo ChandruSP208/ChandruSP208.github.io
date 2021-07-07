278b4c1f43d7325e108bf5dc5024e0495cdf9798
ChandruSP208 <chandru636809@gmail.com>
2021-07-07 22:28:22 +0530

    uml js

--- mod5/script.js
+++ mod5/script.js
@@ -1,7 +1,10 @@
 /*console.log(document.getElementById("title"));
 console.log(document instanceof HTMLDocument);
 */
-function Sayhello(){
+document.addEventListener("DOMContentLoaded",
+function (event){
+function Sayhello(event){
+    this.textContent="said it!"
 	var name=document.getElementById("name").value
    var message="<h2>Hello "+name+"!</h2>";
 
@@ -17,3 +20,18 @@ function Sayhello(){
    	.textContent=title;
    }
 }
+//unobstructive event binding
+document.querySelector("button")
+.addEventListener("click",Sayhello);
+document.querySelector("button").onclick=Sayhello;
+document.querySelector("body")
+.addEventListener("mousemove",
+    function (event){
+        if(event.altKey===true){
+            console.log("x:"+event.clientX);
+             console.log("y:"+event.clientY);
+        }
+    }
+);
+}
+);
